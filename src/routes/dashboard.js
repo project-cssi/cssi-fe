e93504f34a6a5098c32f89d4da214e7124a14303
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-16 18:11:47 +0530

    chore(routes): add application view to the dashboard routes

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,12 +1,17 @@
 import {
-  DashboardView
+  DashboardView,
+  ApplicationsView
 } from '../views';
 import testIcon from '../assets/img/icons/test.svg';
 import dashboardIcon from '../assets/img/icons/dashboard.svg';
+import hmdIcon from '../assets/img/icons/hmd.svg';
 
 const dashboard = [
   {
     path: '/dashboard', name: 'Dashboard', icon: dashboardIcon, component: DashboardView, restrictionLevel: 0,
+  },
+  {
+    path: '/applications', name: 'Applications', icon: hmdIcon, component: ApplicationsView, restrictionLevel: 0,
   },
   {
     path: '/sessions', name: 'Sessions', icon: testIcon, component: DashboardView, restrictionLevel: 0,
